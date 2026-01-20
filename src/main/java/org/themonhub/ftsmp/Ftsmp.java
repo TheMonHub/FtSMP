4c03300c8b27c073938fee9e7db7063b76121893
TheMonHub <mono15.parnthakote@gmail.com>
2026-01-20 23:20:53 +0700

    Init

---
+++ src/main/java/org/themonhub/ftsmp/Ftsmp.java
@@ -0,0 +1,10 @@
+package org.themonhub.ftsmp;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Ftsmp implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
